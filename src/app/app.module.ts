f748955bc62488efe6b7da29a4d5269c405f4414
ZHAO Zinan <zinanzhao2-c@my.cityu.edu.hk>
2018-12-26 10:52:08 +0800

    update about page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
 import { AboutPage } from '../pages/about/about';
+import { EmotionTrend } from '../pages/about/emotionLineChart/trend';
+import { EmotionSuggestion } from '../pages/about/emotionSuggestion/suggestion';
 import { ProfilePage } from '../pages/profile/profile';
 import { ChatPage } from '../pages/chat/chat';
 import { TabsPage } from '../pages/tabs/tabs';
@@ -11,16 +13,21 @@ import { TabsPage } from '../pages/tabs/tabs';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { NgxEchartsModule } from 'ngx-echarts';
+
 @NgModule({
   declarations: [
     MyApp,
     AboutPage,
+    EmotionTrend,
+    EmotionSuggestion,
     ProfilePage,
     ChatPage,
     TabsPage
   ],
   imports: [
     BrowserModule,
+    NgxEchartsModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
